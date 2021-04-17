3d3938e60a9b6c7e8baa1f07577d8e6a1f55c473
OM Miles <olivia.m.miles@gmail.com>
2021-04-17 16:21:40 -0700

    POST & PATCH work for each endpoint, excluding users.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+let userName = `User Name Test`
+let userEmail = `useremailtest@xyz.com`
+
+submitData = (userName, userEmail) => {
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    };
+    return fetch("http://localhost:3000/users/", configObj)
+    .then(resp => resp.json())
+    .then((object) => { 
+        const body = document.querySelector('body');
+        body.innerHTML = object.id;
+        body.style.fontSize = `100px`
+    })
+    .catch((error) => {
+        console.log(error.message)
+        p.textContent = error.message
+        body.appendChild(p)
+    })
+}
+
+submitData()
